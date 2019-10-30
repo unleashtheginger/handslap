22e43a7493183fc6c0f3b71c1e7fa15a2a482b27
Will Ellwood <fragmad@gmail.com>
2019-10-30 20:39:08 +0000

    Risky support for running as root.

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Handslap',
+      version='0.2',
+      description='Website Distraction Counter',
+      author='Will Ellwood',
+      author_email='fragmad@gmail.com',
+      url='https://github.com/fragmad/handslap',
+      packages=['handslap'],
+     )
+
